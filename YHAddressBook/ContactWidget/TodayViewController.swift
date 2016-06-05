89d328306296054f013f6c3367fc5e7e2816c50e
Detailscool <496389006@qq.com>
2016-06-05 15:17:18 +0800

    ADD

---
+++ YHAddressBook/ContactWidget/TodayViewController.swift
@@ -0,0 +1,100 @@
+//
+//  TodayViewController.swift
+//  ContactWidget
+//
+//  Created by Detailscool on 16/6/5.
+//  Copyright © 2016年 Detailscool. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "userDefaultsDidChange:", name: NSUserDefaultsDidChangeNotification, object: nil)
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        update()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+
+        completionHandler(NCUpdateResult.NewData)
+    }
+    
+    func widgetMarginInsetsForProposedMarginInsets(defaultMarginInsets: UIEdgeInsets) -> UIEdgeInsets {
+        return UIEdgeInsetsZero
+    }
+    
+    private func userDefaultsDidChange(notification:NSNotification) {
+        update()
+    }
+    
+    private func update() {
+        
+        for subview in view.subviews {
+            subview.removeFromSuperview()
+        }
+        
+        let sharedDefault = NSUserDefaults(suiteName: "group.cn.hehe.YHAddressBook")
+        let arr = sharedDefault?.objectForKey("Dates") as? [String]
+        
+        let margin :CGFloat = 20.0
+        let count : CGFloat = 5.0
+        let width = (UIScreen.mainScreen().bounds.width-(count+1)*margin)/count
+        
+        if let _ = arr {
+            for (index,dateStr) in arr!.enumerate() {
+                let button = UIButton()
+                button.layer.borderWidth = 1
+                button.layer.borderColor = UIColor.blueColor().CGColor
+                button.titleLabel?.numberOfLines = 0
+                button.titleLabel?.font = UIFont.systemFontOfSize(14)
+                button.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+                button.frame = CGRect(x: ((width + margin) * CGFloat(index%Int(count))) + margin, y: ((width + margin) * CGFloat(index/Int(count))) + margin, width: width, height: width)
+                button.tag = index + 1000
+                button.addTarget(self, action: "buttonClick:", forControlEvents: UIControlEvents.TouchUpInside)
+                let contact = sharedDefault?.objectForKey(String(format: "YHContact_%@", dateStr)) as! String
+                button.setTitle(contact, forState: UIControlState.Normal)
+                view.addSubview(button)
+                
+                if index == arr!.count-1 {
+                    preferredContentSize = CGSizeMake(UIScreen.mainScreen().bounds.height, CGRectGetMaxY(button.frame)+20)
+                }
+            }
+        }
+    }
+    
+    @objc private func buttonClick(button:UIButton) {
+        let sharedDefault = NSUserDefaults(suiteName: "group.cn.hehe.YHAddressBook")
+        let arr = sharedDefault?.objectForKey("Dates") as? [String]
+        if let _ = arr {
+            let index = button.tag - 1000
+            let dateStr = arr![index] 
+            let phone = sharedDefault?.objectForKey(String(format: "YHContactPhone_%@", dateStr)) as! String
+            let url = NSURL(string: String(format: "tel:%@", phone))
+            if let _ = url {
+                self.extensionContext?.openURL(url!, completionHandler: nil)
+            }
+        }
+    }
+    
+}
---
+++ YHAddressBook/YHAddressBook/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  YHAddressBook
+//
+//  Created by Detailscool on 16/6/5.
+//  Copyright © 2016年 Detailscool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameTextfield: UITextField!
+    @IBOutlet weak var phoneTextfield: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBOutlet weak var sureButton: UIButton!
+
+    @IBAction func sure() {
+        
+        sureButton.enabled = false
+        
+        let sharedDefault = NSUserDefaults(suiteName: "group.cn.hehe.YHAddressBook")
+        
+        let date = NSDate()
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let dateStr = formatter.stringFromDate(date)
+        
+        var arr = sharedDefault?.objectForKey("Dates") as? [String]
+        if let _ = arr {
+           arr!.append(dateStr)
+        }else {
+            arr = [String]()
+            arr!.append(dateStr)
+        }
+        
+        sharedDefault?.setValue(nameTextfield.text, forKeyPath: String(format: "YHContact_%@", dateStr))
+        sharedDefault?.setValue(phoneTextfield.text, forKey: String(format: "YHContactPhone_%@", dateStr))
+        sharedDefault?.setObject(arr, forKey: "Dates")
+        sharedDefault?.synchronize()
+        
+    }
+
+}
+
